4ebcc9a71781085c699d1e9c8eb04a857ac5d84c
HaloicCa <hungnvhe187005@fpt.edu.vn>
2026-01-23 17:51:58 +0700

    test header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,12 @@ import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 
-// 1. font inter cho nội dung chung
 const inter = Inter({
     subsets: ["latin"],
     variable: "--font-inter",
     display: "swap",
 });
 
-// 2. font playfair cho tiêu đề
 const playfair = Playfair_Display({
     subsets: ["latin"],
     variable: "--font-playfair",
@@ -17,7 +15,7 @@ const playfair = Playfair_Display({
 });
 
 export const metadata: Metadata = {
-    title: "Au Lac - Vietnamese Eatery",
+    title: "Bamee Gasstro - Vietnamese Eatery",
     description: "The pinnacle of Vietnamese culinary art.",
 };
 
