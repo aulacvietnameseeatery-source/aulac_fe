d050ec261d5a168593a1f93d136d3d9dc93af4bb
HaloicCa <hungnvhe187005@fpt.edu.vn>
2026-03-02 20:33:39 +0700

    change menu, header

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,19 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "placehold.co" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "images.unsplash.com" },
+      {
+        protocol: "https",
+        hostname: "localhost",
+        port: "7083",
+        pathname: "/uploads/**"
+      },
     ],
   },
 };
