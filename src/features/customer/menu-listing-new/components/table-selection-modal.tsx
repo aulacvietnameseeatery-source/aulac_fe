2b9af3460d2415cffa2f8d2b1612188cd7127828
HaloicCa <hungnvhe187005@fpt.edu.vn>
2026-03-10 15:44:30 +0700

    hot fix qr

--- src/features/customer/menu-listing-new/components/table-selection-modal.tsx
+++ src/features/customer/menu-listing-new/components/table-selection-modal.tsx
@@ -2,11 +2,11 @@
 
 import { motion, AnimatePresence } from "framer-motion";
 import { useState, useEffect } from "react";
-import { cn } from "@/lib/utils";
 import { UtensilsCrossed, X, Camera, ChevronRight, RefreshCcw } from "lucide-react";
-import { api } from "@/lib/http";
-import type { ApiResponse } from "@/types/api-response.types";
-import { Scanner } from '@yudiel/react-qr-scanner'; // IMPORT THƯ VIỆN CAMERA VÀO ĐÂY
+import { Scanner } from '@yudiel/react-qr-scanner';
+import {ApiResponse} from "../../../../types/api-response.types";
+import {api} from "../../../../lib/http";
+import {cn} from "../../../../lib/utils";
 
 interface AvailableTableDto {
     tableId: number;
