cf8ce8b27a004d799d82123711152f89ae8b4204
tuanha2004 <hatuan0504@gmail.com>
2026-03-18 01:45:58 +0700

    add, edit, detail customer

--- src/features/staff/customer-management/services/customer-service.ts
+++ src/features/staff/customer-management/services/customer-service.ts
@@ -1,5 +1,5 @@
 import { ApiResponse, PagedResult } from "@/types/api-response.types";
-import { CustomerListDto, CustomerListQueryDto } from "../types/customer-types";
+import { CustomerListDto, CustomerListQueryDto, CustomerDetailDto, CreateCustomerRequest, UpdateCustomerRequest } from "../types/customer-types";
 import { api } from "@/lib/http";
 
 export const staffCustomerService = {
@@ -10,7 +10,6 @@ export const staffCustomerService = {
 
         if (params.search) query.append("search", params.search);
         
-        // Xử lý filter IsMember (chỉ gửi lên nếu khác "All")
         if (params.isMember !== undefined && params.isMember !== "All") {
             query.append("isMember", params.isMember.toString());
         }
@@ -20,5 +19,30 @@ export const staffCustomerService = {
 
         const response = await api.get<ApiResponse<PagedResult<CustomerListDto>>>(`/api/customers?${query.toString()}`);
         return response.data;
-    }
+    },
+
+    getById: async (id: number): Promise<CustomerDetailDto> => {
+        const response = await api.get<ApiResponse<CustomerDetailDto>>(`/api/customers/${id}`);
+        return response.data;
+    },
+
+    createCustomer: async (request: CreateCustomerRequest): Promise<CustomerDetailDto> => {
+        const response = await api.post<ApiResponse<CustomerDetailDto>, CreateCustomerRequest>(
+            `/api/customers`,
+            request
+        );
+        return response.data;
+    },
+
+    updateCustomer: async (id: number, request: UpdateCustomerRequest): Promise<CustomerDetailDto> => {
+        const response = await api.put<ApiResponse<CustomerDetailDto>, UpdateCustomerRequest>(
+            `/api/customers/${id}`,
+            request
+        );
+        return response.data;
+    },
+
+    deleteCustomer: async (id: number): Promise<void> => {
+        await api.delete(`/api/customers/${id}`);
+    },
 };
