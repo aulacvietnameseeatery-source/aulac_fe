b93a7cbbada86edb7d448f5030ed0af8fa340eaa
Quan Truong <158136735+tmq3007@users.noreply.github.com>
2026-03-27 13:44:31 +0700

    refactor: fix indentation and update dine-in link to reservation page in dining-option

--- src/features/customer/reservation/services/table.service.ts
+++
@@ -1,8 +0,0 @@
-import { ZoneTables } from '../types/reservation.types';
-
-export const getTables = async (
-): Promise<ZoneTables> => {
-  // TODO: replace by real API
-  const { ZONES_DATA } = await import('../data/zones.data');
-  return ZONES_DATA;
-};
